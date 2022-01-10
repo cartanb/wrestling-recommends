ee54a9b3541dbd5ebb5db53f8f30a23cdfd7dc21
Brian Cartan <cartanb@gmail.com>
2022-01-10 12:36:06 -0500

    Match results contain matchguide ID

--- client/components/Results.js
+++ client/components/Results.js
@@ -9,19 +9,23 @@ const Results = () => {
   const loadMore = (evt) => {
     evt.preventDefault();
     setPage(page + 1);
-  }
+  };
 
   useEffect(() => {
     setPage(1);
-  }, [results[0]])
+  }, [results[0][0]]);
 
   return (
     <div id="results">
       <ul>
         {results.slice(0, 10 + 5 * (page - 1)).map((match) => (
-          <li key={match}>{match}</li>
+          <li key={match[0]}>
+            <a href={`https://www.cagematch.net/?id=111&nr=${match[1]}`} target="_blank" rel="noopener noreferrer">{decodeURI(match[0])}</a>
+          </li>
         ))}
-        <a href="#" onClick={loadMore}>Load More</a>
+        <a href="#" onClick={loadMore}>
+          Load More
+        </a>
       </ul>
     </div>
   );
